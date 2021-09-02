8fdb29ebcb16d98a3be8492a4c2218e922d1d95b
taylorsieling <taylorasieling@gmail.com>
2021-09-01 22:17:21 -0500

    CORS issue

--- src/apiRoot.js
+++ src/apiRoot.js
@@ -1,3 +1,3 @@
-const DEV_URL = 'http://localhost:3000/';
+const DEV_URL = 'http://localhost:3001/';
 const PROD_URL = 'https://knittinkittenfoster.herokuapp.com/';
 export const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
